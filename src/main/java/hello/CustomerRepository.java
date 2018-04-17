dd4483b328697eaafe6f0eefa9b2dc3c767aafea
Richard Lum <richardlum13@gmail.com>
2018-04-16 23:07:13 -0700

    added @RepositoryRestResource  for autogenerated rest resource access to repository

--- src/main/java/hello/CustomerRepository.java
+++ src/main/java/hello/CustomerRepository.java
@@ -3,8 +3,13 @@ package hello;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+@RepositoryRestResource(collectionResourceRel = "customer", path = "customer")
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+//public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
     List<Customer> findByLastName(String lastName);
 }
